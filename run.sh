e6fdc478a51a4d2388a79be107bacfaab39837da
Mateusz Biel <matbiel@gmail.com>
2023-12-04 18:28:51 +0100

    Install ingress-nginx, aws-load-balancer-controller and kubernetes dashboard

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 export AWS_REGION="eu-west-1"
 export AWS_PAGER=""
 FLUENT_BIT_STACK="fluent-bit-role"
+LB_CONTROLLER_STACK="load-balancer-controller-role"
 
 function deploy() {
   pushd live/demo-cluster
@@ -28,6 +29,10 @@ function get_oidc_id() {
       --output text | cut -d '/' -f 5)
 }
 
+function get_cluster_vpc_id() {
+    vpc_id=$(aws eks describe-cluster --name demo-cluster | jq -r '.cluster.resourcesVpcConfig.vpcId')
+}
+
 function get_stack_outputs() {
   outputs=$(aws cloudformation describe-stacks --stack-name "$1" | jq -r '.Stacks[0].Outputs | INDEX(.OutputKey)')
 }
@@ -46,7 +51,7 @@ function install_fluent_bit() {
   local role_arn
   role_arn=$(jq -r '.RoleArn.OutputValue' <<< "$outputs")
   kubeconfig
-  kubectl create namespace "$namespace"
+  kubectl create namespace "$namespace" --dry-run=client -o yaml | kubectl apply -f -
   export CLUSTER_NAME="demo-cluster"
   envsubst < cluster-info.yaml | kubectl apply -f -
   helm repo add fluent https://fluent.github.io/helm-charts
@@ -88,12 +93,87 @@ function uninstall_fluent_bit() {
   delete_stack "${FLUENT_BIT_STACK}"
 }
 
+function install_load_balancer_controller() {
+  pushd extras/load-balancer-controller
+  local namespace="kube-system"
+  local service_account_name="aws-load-balancer-controller"
+  get_oidc_id
+  aws cloudformation deploy \
+    --template-file iam.yaml \
+    --stack-name "${LB_CONTROLLER_STACK}" \
+    --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" \
+    --parameter-overrides "Namespace=${namespace}" "ServiceAccount=${service_account_name}" "OidcId=${oidc_id}"
+  get_stack_outputs "${LB_CONTROLLER_STACK}"
+  local role_arn
+  role_arn=$(jq -r '.RoleArn.OutputValue' <<< "$outputs")
+read -r -d '\0' service_account << EOM
+{
+  "create": true,
+  "name": "aws-load-balancer-controller",
+  "annotations": {
+    "eks.amazonaws.com/role-arn": "${role_arn}"
+  }
+}
+\0
+EOM
+  get_cluster_vpc_id
+  helm repo add eks https://aws.github.io/eks-charts
+  helm upgrade --install aws-load-balancer-controller eks/aws-load-balancer-controller \
+    --namespace "${namespace}" \
+    --set clusterName="demo-cluster" \
+    --set region="${AWS_REGION}" \
+    --set vpcId="${vpc_id}" \
+    --set-json "serviceAccount=${service_account}"
+  popd
+}
+
+function uninstall_load_balancer_controller() {
+   helm uninstall aws-load-balancer-controller --namespace "kube-system" --ignore-not-found
+   delete_stack "${LB_CONTROLLER_STACK}"
+}
+
+function install_ingress_nginx() {
+  pushd extras/ingress-nginx
+  local namespace="ingress-nginx"
+  helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+  kubectl create namespace "$namespace" --dry-run=client -o yaml | kubectl apply -f -
+  helm upgrade --install ingress-nginx ingress-nginx/ingress-nginx \
+    -f nlb-service.yaml \
+    --namespace "${namespace}"
+  popd
+}
+
+function install_dashboard() {
+  helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+  helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard \
+    --create-namespace --namespace kubernetes-dashboard \
+    --set nginx.enabled=false \
+    --set cert-manager.enabled=false \
+    --set app.ingress.enabled=false \
+    --set extraArgs="{--enable-skip-login, --disable-settings-authorizer, --enable-insecure-login}"
+}
+
+function uninstall_dashboard() {
+   helm uninstall kubernetes-dashboard --ignore-not-found
+}
+
+function uninstall_ingress_nginx() {
+   helm uninstall ingress-nginx --ignore-not-found
+   kubectl delete namespace "ingress-nginx" --ignore-not-found=true
+}
+
 function install() {
   install_fluent_bit
+  install_load_balancer_controller
+  install_ingress_nginx
+  install_dashboard
 }
 
 function uninstall() {
   uninstall_fluent_bit
+  uninstall_ingress_nginx
+  uninstall_load_balancer_controller
+  uninstall_dashboard
 }
 
 case "$1" in
